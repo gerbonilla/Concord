32d52401429e5387bbf7564fd47b1ae35d4bf272
Gerardo Bonilla <gerbonilla@gmail.com>
2017-09-26 17:45:03 +0100

    more updates

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -2,7 +2,31 @@ class ProfilesController < ApplicationController
   before_action :set_profile, only: [:show, :edit, :update]
 
   def show
-    @quovo_accounts = Quovo.accounts.for_user(current_user.quovo_user_id)
+
+    # Build accounts array
+    @accounts = []
+    Quovo.accounts.for_user(current_user.quovo_user_id).each do |account|
+      portfolios = []
+      Quovo.portfolios.for_account(account.id).each do |portfolio|
+        positions = []
+        asset_class = {}
+        Quovo.positions.for_portfolio(portfolio.id).each do |position|
+          positions << position
+          asset_class[position.asset_class].nil? ? asset_class[position.asset_class] = position.value : asset_class[position.asset_class] += position.value
+        end
+        portfolios << {portfolio: portfolio, positions: positions, asset_class: asset_class}
+      end
+      @accounts << {account: account, portfolios: portfolios}
+    end
+
+    # Build positions array
+    @positions = {}
+    Quovo.positions.for_user(current_user.quovo_user_id).each do |p|
+      @positions[p.asset_class].nil? ? @positions[p.asset_class] = p.value : @positions[p.asset_class] += p.value
+    end
+
+    # Build asset_class array per account
+
     @token = Quovo.iframe_token.create(current_user.quovo_user_id).token
     @quovo_user = current_user.quovo_user_id
   end
